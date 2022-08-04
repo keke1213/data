0c787b96969c7bbf0aa2f911aa3fc4736ddaf176
keke1213 <1043704423@qq.com>
2022-08-04 13:45:54 +0800

    version 1.0.1

---
+++ sparesArry/src/com/leetcode/leetcode899_有序队列.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+import java.util.*;
+
+public class leetcode899_有序队列 {
+    public static void main(String[] args) {
+//        orderlyQueue("cba",1);
+        orderlyQueue("baaca",3);
+    }
+    public static String orderlyQueue(String s, int k) {
+        if (k == 1) {
+            String smallest = s;
+            StringBuilder sb = new StringBuilder(s);
+            int n = s.length();
+            for (int i = 1; i < n; i++) {
+                char c = sb.charAt(0);
+                sb.deleteCharAt(0);
+                sb.append(c);
+                if (sb.toString().compareTo(smallest) < 0) {
+                    smallest = sb.toString();
+                }
+            }
+            return smallest;
+        } else {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            return new String(arr);
+        }
+    }
+}
