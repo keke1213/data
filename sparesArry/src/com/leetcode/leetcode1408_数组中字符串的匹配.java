581ef1b40d1421105e00e7d9e2570285ad8b9fe8
keke1213 <1043704423@qq.com>
2022-08-06 12:59:09 +0800

    iuss53 1.0.02

--- sparesArry/src/com/leetcode/leetcode1408_数组中字符串的匹配.java
+++ sparesArry/src/com/leetcode/leetcode1408_数组中字符串的匹配.java
@@ -1,4 +1,33 @@
 package com.leetcode;
 
+import java.util.*;
+
 public class leetcode1408_数组中字符串的匹配 {
+    public static void main(String[] args) {
+        stringMatching(new String[]{"mass","as","hero","superhero"});
+    }
+    public static List<String> stringMatching(String[] words) {
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.length()-o1.length();
+            }
+        });
+        List<String> list=new ArrayList<>();
+        HashSet<String > hashSet = new HashSet<>();
+        int length = words.length;
+        if (length<2){
+            return list;
+        }
+        for (int i = 0; i < length-1; i++) {
+            for (int j = i+1; j < length; j++) {
+                if (words[i].contains(words[j])){
+                    hashSet.add(words[j]);
+                }
+            }
+        }
+        list=new ArrayList<>(hashSet);
+
+        return list;
+    }
 }
--- sparesArry/src/com/leetcode/leetcode623_在二叉树中增加一行.java
+++ sparesArry/src/com/leetcode/leetcode623_在二叉树中增加一行.java
@@ -2,6 +2,21 @@ package com.leetcode;
 
 public class leetcode623_在二叉树中增加一行 {
     public static void main(String[] args) {
-        System.out.println(1);
+
+    }
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        return null;
     }
 }
